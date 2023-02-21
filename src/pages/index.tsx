e8f2fe6cd8c06f5e4a08e8eb6df7bd4847a5c469
Vitor <marthendalnunes@gmail.com>
2023-02-21 18:08:56 -0300

    feat: add send transaction example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { Head } from '@/components/layout/Head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -7,9 +8,11 @@ export default function Home() {
       <main>
         <h1 className="mt-10 text-4xl font-bold">Next.js Web3 Starter</h1>
         <h3 className="mt-1 text-lg font-medium">Powered by General Magic</h3>
-        <h2 className="mt-6 text-3xl font-semibold">Api functionalities:</h2>
+        <h2 className="mt-6 text-3xl font-semibold">Examples:</h2>
         <ul className="mt-2 list-inside list-disc text-xl">
-          <li>Connect to wallet</li>
+          <li>
+            <Link href="/examples/send-transaction">Send Transaction</Link>
+          </li>
         </ul>
       </main>
     </>
