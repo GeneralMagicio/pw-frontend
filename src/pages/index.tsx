8d56959053524cc15f671f50070c6459e294b3d3
Vitor <marthendalnunes@gmail.com>
2023-02-21 23:32:36 -0300

    feat: add contract write example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,11 +10,26 @@ export default function Home() {
         <h3 className="mt-1 text-lg font-medium">Powered by General Magic</h3>
         <h2 className="mt-6 text-3xl font-semibold">Examples:</h2>
         <ul className="mt-2 list-inside list-disc text-xl">
-          <li>
-            <Link href="/examples/send-transaction">Send Transaction</Link>
-          </li>
+          {items.map(({ title, url }) => (
+            <li
+              className="transition duration-200 hover:opacity-60"
+              key={title}>
+              <Link href={url}>{title}</Link>
+            </li>
+          ))}
         </ul>
       </main>
     </>
   )
 }
+
+const items = [
+  {
+    title: 'Send Transaction',
+    url: '/examples/send-transaction',
+  },
+  {
+    title: 'Contract Write',
+    url: '/examples/contract-write',
+  },
+]
