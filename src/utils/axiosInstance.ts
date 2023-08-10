be07ed364f3a153a4c96f719e6425c9445109737
Youssef <yussdev@gmail.com>
2023-08-10 16:41:26 +0330

    chore(app): make app env base

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://pairwise.iran.liara.run/',
+  baseURL:
+    process.env.NEXT_PUBLIC_BASE_URL || 'https://pairwise.iran.liara.run',
   withCredentials: true,
   headers: {
     'Content-type': 'application/json',
