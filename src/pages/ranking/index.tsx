e72dcac30dbf82f35080111f0f0999d95760a18c
Mahdi Ghajary <mahdighajary@gmail.com>
2023-09-19 14:15:35 +0330

    Add editing logic for collections and their subunits

--- src/pages/ranking/index.tsx
+++ src/pages/ranking/index.tsx
@@ -4,17 +4,19 @@ import {
 } from '@/components/Poll/Rankings/OverallRanking'
 import { AttestationModal } from '@/components/Poll/Rankings/OverallRankingRow/AttestationModal'
 import { OverallRankingHeader } from '@/components/Poll/Rankings/OverallRankingRow/OverallRankingHeader'
-import { OverallRankingType, Ranking } from '@/types/Ranking/index'
+import { addLockedProperty, changePercentage } from '@/components/Poll/Rankings/edit-logic'
+import { changeCollectionPercentage } from '@/components/Poll/Rankings/edit-logic/collection-editing'
+import { OverallRankingType, Rank } from '@/types/Ranking/index'
 import { getOverallRanking } from '@/utils/poll'
 import { useEffect, useState } from 'react'
 import { useChainId, useSwitchNetwork } from 'wagmi'
 
-const flattenRankingData = (ranking: OverallRankingType[]): Ranking[] => {
+const flattenRankingData = (ranking: OverallRankingType[]): Rank[] => {
   return ranking.reduce((acc, item) => {
     if (hasSubcollections(item.ranking)) {
       return [...acc, ...flattenRankingData(item.ranking)]
     } else return [...acc, ...item.ranking]
-  }, [] as Ranking[])
+  }, [] as Rank[])
 }
 
 export default function RankingPage() {
@@ -29,9 +31,18 @@ export default function RankingPage() {
     main()
   }, [setRankings])
 
+
+  const change = () => {
+    if (!rankings) return;
+    const editRanking = addLockedProperty(rankings)
+    const newRanking = changeCollectionPercentage(editRanking, 5, 0.3)
+
+    console.log("new Ranking:", newRanking)
+  }
+
   return (
     <>
-      <OverallRankingHeader onDone={() => {setOpen(true)}} onEdit={() => {}} />
+      <OverallRankingHeader onDone={() => {change(); setOpen(true)}} onEdit={() => {}} />
       {isOpen && <AttestationModal
         isOpen={isOpen}
         onClose={() => setOpen(false)}
