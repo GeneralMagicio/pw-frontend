8a094b0bbbc03cc60f5ca1071f2c29dfc654e2fb
Mahdi Ghajary <mahdighajary@gmail.com>
2023-10-18 15:16:36 +0330

    Fixing the open graph image now showing up

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,7 +2,7 @@ import { optimism, optimismGoerli } from '@wagmi/chains'
 
 export const SITE_NAME = 'Pairwise'
 export const SITE_DESCRIPTION = 'Pairwise by General Magic.'
-export const SITE_URL = 'https://pairwise.generalmagic.io/'
+export const SITE_URL = 'https://staging.pairwise.vote'
 
 export const SOCIAL_TWITTER = 'generalmagicio'
 export const SOCIAL_GITHUB = 'GeneralMagicio/pairwise'
