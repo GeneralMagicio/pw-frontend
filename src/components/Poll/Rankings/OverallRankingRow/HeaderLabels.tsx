36d7acf5eabac7a092b6260889436d10f76e2409
Kristofer <kristofer@fmckl.se>
2023-11-03 16:00:51 +0100

    Progress percentage working again

--- src/components/Poll/Rankings/OverallRankingRow/HeaderLabels.tsx
+++ src/components/Poll/Rankings/OverallRankingRow/HeaderLabels.tsx
@@ -19,28 +19,11 @@ export const HeaderLabels: React.FC<HeaderLabelProps> = ({
     )
   }
 
-  // if (!pairs) return null
-
-  const progressPercentage = 10
-  // const progressPercentage = pairs.totalPairs
-  //   ? Math.max((pairs.votedPairs / pairs.totalPairs) * 100, 4)
-  //   : 4
-  // const minVotesToUnlock = Math.ceil(pairs.totalPairs * pairs.threshold)
-  // const voteCountsToUnklock = minVotesToUnlock - pairs.votedPairs
-
-  if (progress === 'WIP') {
-    return (
-      <span className="flex w-fit items-center whitespace-nowrap rounded-3xl bg-gray-100 p-1 px-2 text-xs text-[#1C64F2]">
-        In progress {progressPercentage.toFixed(0)}%
-      </span>
-    )
-  }
-
   if (progress === 'Finished') {
     return (
       <div className="flex gap-2">
         <span className="flex w-fit items-center whitespace-nowrap rounded-3xl bg-[#22B7A01A] p-1 px-2 text-xs text-[#22B7A0]">
-          Ranked <Check className="ml-1 h-3 w-3" color="#22B7A0" />
+          Ranked <Check className="w-3 h-3 ml-1" color="#22B7A0" />
         </span>
         <span className="flex w-fit items-center whitespace-nowrap rounded-3xl bg-[#F366001A] p-1 px-2 text-xs text-[#F36600]">
           List not created
@@ -56,6 +39,20 @@ export const HeaderLabels: React.FC<HeaderLabelProps> = ({
           List created
         </span>
       </div>
+    )
+  }
+
+  if (!pairs) return null
+
+  const progressPercentage = pairs.totalPairs
+    ? Math.max((pairs.votedPairs / pairs.totalPairs) * 100, 4)
+    : 4
+
+  if (progress === 'WIP') {
+    return (
+      <span className="flex w-fit items-center whitespace-nowrap rounded-3xl bg-gray-100 p-1 px-2 text-xs text-[#1C64F2]">
+        In progress {progressPercentage.toFixed(0)}%
+      </span>
     )
   }
 
