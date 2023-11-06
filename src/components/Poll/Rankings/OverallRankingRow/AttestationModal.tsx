a89d7565c161b96fa819352b0d1cab98847effaa
Mahdi Ghajary <mahdighajary@gmail.com>
2023-11-06 17:55:13 +0330

    Remove the previous attestation modal

--- src/components/Poll/Rankings/OverallRankingRow/AttestationModal.tsx
+++ src/components/Poll/Rankings/OverallRankingRow/AttestationModal.tsx
@@ -40,6 +40,8 @@ export const AttestationModal: React.FC<Props> = ({
   const [step, setSteps] = useState<number>(0)
   const [loading, setLoading] = useState(false)
   const [url, setUrl] = useState<string>()
+  const [agoraUrl, setAgoraUrl] = useState('')
+  const [smUrl, setSmUrl] = useState('')
 
   const progress = Math.ceil(((step - 1) / 3) * 100)
 
@@ -50,7 +52,7 @@ export const AttestationModal: React.FC<Props> = ({
 
   const signer = useSigner()
 
-  const handleSignClick = async () => {
+  const handleCreate = async () => {
     setLoading(true)
     try {
       await attest()
@@ -107,21 +109,29 @@ export const AttestationModal: React.FC<Props> = ({
     ])
 
     try {
+
       const tx = await eas.attest({
         schema: SCHEMA_UID,
         data: {
           data: encodedData,
           recipient: address,
-          revocable: false,
+          revocable: true,
         },
       })
 
+      // const newAttestationUID = ""
       const newAttestationUID = await tx.wait()
       await axiosInstance.post('/flow/reportAttest', {
         cid: collectionId,
       })
       setUrl(`${easConfig.explorer}/attestation/view/${newAttestationUID}`)
-      setSteps(3)
+      setAgoraUrl(
+        `https://optimism-agora-dev.agora-dev.workers.dev/retropgf/3/list/${newAttestationUID}`
+      )
+      setSmUrl(
+        `https://retro-pgf-staging.vercel.app/lists/${newAttestationUID}`
+      )
+      setSteps(1)
     } catch (e) {
       console.error('error on sending tx:', e)
       // setUrl(
@@ -134,24 +144,10 @@ export const AttestationModal: React.FC<Props> = ({
     <Modal isOpen={isOpen} onClose={onClose}>
       <div
         className={cn(
-          'relative flex min-h-[400px] w-[600px] flex-col gap-10 py-8 px-2 font-IBM',
-          { 'min-h-[280px]': step === 4 }
+          'relative flex min-h-[250px] w-[600px] flex-col gap-10 py-8 px-2 font-IBM',
         )}>
-        <header className="absolute top-0 mb-2 flex w-full justify-between">
-          {step === 0 || step === 4 ? (
-            <h2 className="text-3xl font-bold">Create list</h2>
-          ) : (
-            <div />
-          )}
-          <Close className="cursor-pointer" onClick={onClose} />
-        </header>
-        {step > 0 && isLessThanLastStep && (
-          <div className="mt-4">
-            <LinearProgress progress={progress} />
-          </div>
-        )}
         {step === 0 && (
-          <div className="flex flex-col gap-10 mt-10">
+          <div className="mt-10 flex flex-col gap-10">
             <p className="text-xl">
               Lists that you create here can be used for the RetroPGF voting
               both in Agora and Supermodular.
@@ -162,7 +158,7 @@ export const AttestationModal: React.FC<Props> = ({
               Ethereum Attestation Service. You need to make a transaction on
               Optimism to create the list.
             </p>
-            <div className="flex text-sm justify-between py-3 px-4 rounded-2xl text-[#F36600] bg-[#F366001A]">
+            <div className="flex justify-between rounded-2xl bg-[#F366001A] py-3 px-4 text-sm text-[#F36600]">
               <p>Create list using EAS on Optimism.</p>
               <p>{`Estimated cost < 0.000x$`}</p>
             </div>
@@ -176,94 +172,38 @@ export const AttestationModal: React.FC<Props> = ({
                 className={
                   'flex h-12 w-fit items-center self-center rounded-full bg-black px-8 py-2  text-white'
                 }
-                onClick={() => setSteps(1)}>
-                Create list
+                onClick={handleCreate}>
+                {loading ? "Loading..." : "Create list"}
               </button>
             </div>
           </div>
         )}
-        {step > 0 && isLessThanLastStep && (
-          <div className="flex w-[80%] items-center">
-            <p className="w-16"> 1- </p>
-            <p className="w-[75%]">
-              Are you sure you want to attest to the ranking result available in
-              this page?
-            </p>
-            <button
-              className="ml-8 h-12 w-32 rounded-lg bg-gray-600 text-white"
-              onClick={() => setSteps(2)}>
-              {step > 1 ? 'Confirmed' : 'Confirm'}
-            </button>
-          </div>
-        )}
-        {step > 1 && isLessThanLastStep && (
-          <div className="flex w-[80%] items-center">
-            <p className="w-16"> 2- </p>
-            <p className="w-[75%]"> Please sign the transaction.</p>
-            <button
-              className="ml-8 h-12 w-32 rounded-lg bg-gray-600 text-white"
-              onClick={handleSignClick}>
-              {loading ? (
-                <CircularProgress color="#4b5563" isIndeterminate size={28} />
-              ) : step > 2 ? (
-                'Signed'
-              ) : (
-                'Sign'
-              )}
-            </button>
-          </div>
-        )}
-        {step > 2 && isLessThanLastStep && (
-          <div className="flex w-[80%] items-center">
-            <p className="w-16"> 3- </p>
-            <p className="w-[75%]"> Check out your attestations:</p>
-            <a
-              className="ml-8 flex w-32 justify-center rounded-lg bg-gray-600 py-3 text-white"
-              href={url}
-              onClick={() => setSteps(4)}
-              rel="noreferrer"
-              target="_blank">
-              <button>View</button>
-            </a>
-          </div>
-        )}
-        {step > 3 && isLessThanLastStep && (
-          <Link
-            className="ml-8 flex w-32 justify-center self-center rounded-lg bg-gray-600 py-3 text-white"
-            href="/galaxy">
-            <button>Done</button>
-          </Link>
-        )}
-        {step === 4 && (
-          <div className="flex flex-col gap-10 mt-10">
+        
+        {step === 1 && (
+          <div className="mt-10 flex flex-col gap-10">
             <p className="text-xl">
-              The list has been created, you can access it now on Agora or
+              The list has been created, you can access it shortly on Agora or
               Supermodular.
             </p>
-            <div className="flex flex-col gap-4 justify-center items-center">
-              <button
-                className="flex h-[50px] items-center justify-center rounded-full border border-black p-2 px-8 text-sm"
-                onClick={() => {}}>
-                View list on Agora
-                <LinkSharp className="ml-4" />
-              </button>
-              <button
-                className="flex h-[50px] items-center justify-center rounded-full border border-black p-2 px-8 text-sm"
-                onClick={() => {}}>
-                View list on Supermodular
-                <LinkSharp className="ml-4" />
-              </button>
+            <div className="flex flex-col items-center justify-center gap-4">
+              <a href={agoraUrl} rel="noreferrer" target='_blank'>
+                <button
+                  className="flex h-[50px] items-center justify-center rounded-full border border-black p-2 px-8 text-sm"
+                  onClick={() => {}}>
+                  View list on Agora
+                  <LinkSharp className="ml-4" />
+                </button>
+              </a>
+              <a href={smUrl} rel="noreferrer" target='_blank'>
+                <button
+                  className="flex h-[50px] items-center justify-center rounded-full border border-black p-2 px-8 text-sm"
+                  onClick={() => {}}>
+                  View list on Supermodular
+                  <LinkSharp className="ml-4" />
+                </button>
+              </a>
             </div>
           </div>
-        )}
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        {step > 0 && isLessThanLastStep && (
-          <img
-            alt="diploma"
-            className="absolute right-[0px] top-[35%] opacity-[15%]"
-            src="/images/diploma.png"
-            width={100}
-          />
         )}
       </div>
     </Modal>
