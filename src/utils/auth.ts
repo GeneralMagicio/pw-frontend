6409d29201600a36d4be7e8e905e6e3941581e6c
Kristofer <kristofer@fmckl.se>
2023-11-07 13:03:22 +0100

    Fixes after PR review

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -8,11 +8,7 @@ axiosInstance.interceptors.response.use(
   },
   function (error) {
     if (error.response && error.response.status === 401) {
-      window.localStorage.removeItem('auth')
-      window.localStorage.removeItem('loggedInAddress')
-      if (axiosInstance.defaults.headers.common['auth']) {
-        delete axiosInstance.defaults.headers.common['auth']
-      }
+      logout()
     }
     return Promise.reject(error)
   }
@@ -39,37 +35,33 @@ const fetchNonce = async () => {
 export const login = async (
   chainId: number,
   address: string,
-  signFunction: (args?: any) => Promise<`0x${string}` | undefined>
+  signFunction: (args?: any) => Promise<`0x${string}`>
 ) => {
-  try {
-    const nonce = await fetchNonce()
+  const nonce = await fetchNonce()
 
-    const message = new SiweMessage({
-      domain: window.location.host,
-      address,
-      statement: 'Sign in with Ethereum to Pairwise.',
-      uri: window.location.origin,
-      version: '1',
-      chainId,
-      nonce,
-    })
+  const message = new SiweMessage({
+    domain: window.location.host,
+    address,
+    statement: 'Sign in with Ethereum to Pairwise.',
+    uri: window.location.origin,
+    version: '1',
+    chainId,
+    nonce,
+  })
 
-    const signature = await signFunction({
-      message: message.prepareMessage(),
-    })
+  const signature = await signFunction({
+    message: message.prepareMessage(),
+  })
 
-    // Verify signature
-    const verifyRes = await axiosInstance.post('/auth/login', {
-      ...{ message, signature },
-    })
+  // Verify signature
+  const verifyRes = await axiosInstance.post('/auth/login', {
+    ...{ message, signature },
+  })
 
-    window.localStorage.setItem('auth', verifyRes.data)
-    window.localStorage.setItem('loggedInAddress', address)
-    axiosInstance.defaults.headers.common['auth'] = verifyRes.data
-    return verifyRes
-  } catch (error) {
-    throw error
-  }
+  window.localStorage.setItem('auth', verifyRes.data)
+  window.localStorage.setItem('loggedInAddress', address)
+  axiosInstance.defaults.headers.common['auth'] = verifyRes.data
+  return verifyRes
 }
 
 export const logout = async () => {
