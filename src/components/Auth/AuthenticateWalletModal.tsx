4b7fca8823207d738cfe401ee11cead49172f97a
Kristofer <kristofer@fmckl.se>
2023-11-09 12:49:51 +0100

    Add disconnect option

--- src/components/Auth/AuthenticateWalletModal.tsx
+++ src/components/Auth/AuthenticateWalletModal.tsx
@@ -1,4 +1,4 @@
-import { useNetwork, useSwitchNetwork } from 'wagmi'
+import { useDisconnect, useNetwork, useSwitchNetwork } from 'wagmi'
 
 import { Close } from '@/components/Icon/Close'
 import { ColoredGrid } from '../Icon/ColoredGrid'
@@ -14,15 +14,21 @@ export const AuthenticateWalletModal: React.FC<
 > = ({ handleClose, handleLogin }) => {
   const { chain } = useNetwork()
   const { switchNetwork } = useSwitchNetwork()
+  const { disconnect } = useDisconnect()
 
   const wrongChain = chain?.id !== 10 // Optimism
 
-  const handleClick = () => {
+  const handleConnecClick = () => {
     if (wrongChain) {
       switchNetwork?.(10)
     } else {
       handleLogin()
     }
+  }
+
+  const handleDisconnectClick = () => {
+    disconnect()
+    handleClose()
   }
 
   return (
@@ -35,15 +41,29 @@ export const AuthenticateWalletModal: React.FC<
           </div>
           <h3 className="text-lg font-bold">Authenticate wallet</h3>
           <p className="text-sm">
-            Please sign the login message with your wallet to continue.
+            Sign the login message with your wallet to continue.
           </p>
-          <button
-            className={'min-w-[120px] rounded-full bg-red px-4 py-2 text-white'}
-            onClick={handleClick}>
-            {wrongChain
-              ? 'Wrong chain, connect to Optimism'
-              : 'Authenticate wallet'}
-          </button>
+          {wrongChain && (
+            <p className="text-sm font-bold text-red">
+              Wrong network, pairwise currently only supports Optimism.
+            </p>
+          )}
+          <div className="flex w-full justify-center gap-5">
+            {wrongChain && (
+              <button
+                className="min-w-[120px] rounded-full border border-red px-4 py-2 text-red"
+                onClick={handleDisconnectClick}>
+                Disconnect
+              </button>
+            )}
+            <button
+              className={
+                'min-w-[120px] rounded-full bg-red px-4 py-2 text-white'
+              }
+              onClick={handleConnecClick}>
+              {wrongChain ? 'Connect to Optimism' : 'Sign in'}
+            </button>
+          </div>
         </div>
       </div>
     </>
