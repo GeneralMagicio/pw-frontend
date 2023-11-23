f2bd663a04f1f0027774ca9498fa805d37addcd0
Kristofer <kristofer@fmckl.se>
2023-11-23 11:42:13 +0100

    Better mobile lock screen

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ import { axiosInstance } from '@/utils/axiosInstance'
 import cn from 'classnames'
 import localFont from 'next/font/local'
 import { useLayoutEffect } from 'react'
+import { useWindowWidth } from '@react-hook/window-size'
 
 const IBMFont = localFont({
   src: [
@@ -62,6 +63,20 @@ export default function App({ Component, pageProps }: AppProps) {
       axiosInstance.defaults.headers.common['auth'] = `${token}`
     }
   }, [])
+
+  const width = useWindowWidth()
+
+  if (width < 1024) {
+    return (
+      <div className="main-layout flex min-h-screen shrink-0 flex-col items-center justify-center bg-cover bg-no-repeat font-IBM text-black">
+        <div className="flex w-96 flex-col gap-3 text-center">
+          <div className="text-xl font-bold">Large screens only</div>
+          Pairwise is not currently available on mobile and tablet devices.
+          Please visit on a desktop or laptop computer.
+        </div>
+      </div>
+    )
+  }
 
   return (
     <>
